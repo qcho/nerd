d5af04ff43c14749379d7ea86239d21b8af29d84
Juan Pablo Orsay <jporsay@gmail.com>
2019-02-02 15:31:53 -0300

    Add missing types from definition

--- daemon/nerd_model.py
+++ daemon/nerd_model.py
@@ -7,3 +7,4 @@ class NerdModel():
     def __init__(self, model: Language, path: Path, entity_types: List[str]):
         self.nlp = model
         self.path = path
+        self.entity_types = entity_types
