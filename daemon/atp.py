52a766ed816361b8abbcf19f91a70ee8661dae7d
Juan Pablo Orsay <jporsay@gmail.com>
2019-02-06 09:52:05 -0300

    Add basic text sanitization

--- daemon/atp.py
+++ daemon/atp.py
@@ -3,6 +3,10 @@ from nerd_model import NerdModel
 from nerd_type_aliases import NEREntity
 from typing import List, Tuple
 
+def sanitize_text(text: str):
+    return text.strip()
+
+
 def parse_text(model: NerdModel, text: str):
     """Parses a text returning the SpaCy JSON representation of it
 
@@ -13,7 +17,7 @@ def parse_text(model: NerdModel, text: str):
     Returns:
         JSON: NLP JSON representation of the given text
     """
-    doc = model.model(text)
+    doc = model.model(sanitize_text(text))
     return doc.to_json()
 
 def train_model(model: NerdModel, train_data: Tuple[str, List[NEREntity]]):
