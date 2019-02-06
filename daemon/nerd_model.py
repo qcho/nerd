0ac22fa92319993dc4965bf2ed6754aa0960138d
Juan Pablo Orsay <jporsay@gmail.com>
2019-02-06 09:51:25 -0300

    Add ModelDirectories

--- daemon/nerd_model.py
+++ daemon/nerd_model.py
@@ -8,11 +8,12 @@ from pathlib import Path
 from typing import List
 
 from model_metadata import ModelMetadata
+from model_directories import ModelDirectories
 
 class NerdModel():
     def __init__(self, name: str, path: Path):
         self.name = name
-        self.path = path
+        self.directories = ModelDirectories(path)
         self.model = None
         self.metadata = ModelMetadata()
 
@@ -28,15 +29,6 @@ class NerdModel():
         self._load_metadata()
         self._load_model()
 
-    def _metadata_path(self):
-        return self.path / 'metadata.json'
-
-    def model_path(self):
-        return self.path / 'model'
-
-    def data_path(self):
-        return self.path / 'data'
-
     def save(self, model=None):
         self._save_metadata()
         self._save_model(model)
@@ -50,13 +42,12 @@ class NerdModel():
     def _load_model(self):
         if not self.model is None:
             return
-        self.model = spacy.load(self.model_path())
+        self.model = spacy.load(self.directories.model_path())
 
     def _save_model(self, model: Language = None):
         if model is not None:
             self.model = model
         if self.model is None:
             raise Exception("Saving a model requires having a model")
-        os.makedirs(self.path)
-        os.makedirs(self.data_path())
-        self.model.to_disk(self.model_path())
+        self.directories.initialize()
+        self.model.to_disk(self.directories.model_path())
