d5684b58b699d823f07396447189780094e4590b
Juan Pablo Orsay <jporsay@gmail.com>
2019-02-24 19:18:22 -0300

    Allowing DELETE in CORS requests

--- daemon/app.py
+++ daemon/app.py
@@ -75,6 +75,8 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers',
                          'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET, PUT, POST, DELETE, HEAD')
     return response
 
 
@@ -85,6 +87,8 @@ def on_http_exception(error):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers',
                          'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET, PUT, POST, DELETE, HEAD')
     return response
 
 
