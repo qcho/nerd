642af8709eaad68c6ebdbac3d537ffcda7e2b246
Juan Pablo Orsay <jporsay@gmail.com>
2019-02-28 20:32:02 -0300

    Model load is lazy

--- daemon/nerd_model.py
+++ daemon/nerd_model.py
@@ -14,12 +14,17 @@ class NerdModel():
     def __init__(self, name: str, path: Path):
         self.name = name
         self.directories = ModelDirectories(path)
-        self.model = None
+        self._model = None
         self.metadata = ModelMetadata()
 
     @property
     def entity_types(self):
         return self.metadata.entity_types
+
+    @property
+    def model(self):
+        self._load_model()
+        return self._model
 
     def upsert_entity_type(self, code, name=None):
         self.metadata.upsert_entity_type(code, name)
@@ -27,10 +32,9 @@ class NerdModel():
 
     def load(self):
         self._load_metadata()
-        self._load_model()
 
     def save(self, model=None):
-        if self.model is None:
+        if self._model is None:
             self.directories.initialize()
         self._save_metadata()
         self._save_model(model)
@@ -42,13 +46,13 @@ class NerdModel():
         self.metadata.save(self.directories.metadata_file())
 
     def _load_model(self):
-        if not self.model is None:
+        if not self._model is None:
             return
-        self.model = spacy.load(self.directories.model_path())
+        self._model = spacy.load(self.directories.model_path())
 
     def _save_model(self, model: Language = None):
         if model is not None:
-            self.model = model
-        if self.model is None:
+            self._model = model
+        if self._model is None:
             raise Exception("Saving a model requires having a model")
-        self.model.to_disk(self.directories.model_path())
+        self._model.to_disk(self.directories.model_path())
