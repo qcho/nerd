45ab558bf12291270ed176d1ceb2d21eee8af226
Juan Pablo Orsay <jporsay@gmail.com>
2019-03-03 10:48:38 -0300

    Remove old class

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,23 +1,17 @@
 from werkzeug.exceptions import Unauthorized, BadRequest, HTTPException, NotFound, Conflict
 
+
 class InvalidBaseModel(BadRequest):
     description = "Invalid base model"
+
 
 class BadCredentials(Unauthorized):
     description = "Invalid credentials"
 
 
-class MissingParameters(BadRequest):
-    def __init__(self, message):
-        super().__init__(self, description=message)
-
-    @classmethod
-    def param_check_failed(cls, key=None, got=None):
-        return cls('Parameter {0} got {1}'.format(key, got))
-
-
 class ModelExists(Conflict):
     description = "Model exists"
+
 
 class ModelNotFound(NotFound):
     description = "Model not found"
