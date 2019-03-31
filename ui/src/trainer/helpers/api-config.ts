93ef2042edfbf8a1adf7faa0e4c9b6d99f2f132a
Juan Pablo Orsay <jporsay@gmail.com>
2019-03-31 16:51:53 -0300

    Create api-config

---
+++ ui/src/trainer/helpers/api-config.ts
@@ -0,0 +1,10 @@
+import CredentialsStorage from "./CredentialsStorage";
+import { Configuration } from "../apigen";
+
+export function apiConfig(): Configuration {
+  const credentials = CredentialsStorage.getStored();
+  if (credentials == null) {
+    return {};
+  }
+  return { accessToken: credentials.access_token };
+}
