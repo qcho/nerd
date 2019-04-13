1822b96f7bd46f829f269da8452281863fcea995
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2019-04-13 19:09:23 -0300

    fix typo

--- app/main.py
+++ app/main.py
@@ -9,11 +9,15 @@ from apis.users import blp as users
 from core.cli import setup_cli
 from core.security import jwt
 from flask import Flask
-from settings import MONGO_CONFIG, configure_app
+from logit import get_logger
+from settings import MONGO_CONFIG, setup_settings
 from tasks import make_celery
 
+logger = get_logger(__name__)
+
 app = Flask('NERd')
-configure_app(app)
+setup_settings(app)
+celery = make_celery(app)
 
 mongoengine.connect(**MONGO_CONFIG)
 jwt.init_app(app)
@@ -25,8 +29,6 @@ api.register_blueprint(users, url_prefix='/api/users')
 api.register_blueprint(roles, url_prefix='/api/roles')
 
 setup_cli(app)
-
-celery = make_celery(app)
 
 if __name__ == '__main__':
     app.run()
