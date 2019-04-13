2855e0b1bae7d5e947d48f44bd57fef8ecde5e5e
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2019-04-13 16:38:08 -0300

    move config to their own rightful file

--- app/main.py
+++ app/main.py
@@ -1,89 +1,23 @@
 import os
 
 import mongoengine
-from flask import Flask
-
-from core.document.user import Role
-from core.security import jwt
-from core.cli import setup_cli
 from apis import api
 from apis.auth import blp as auth
 from apis.corpora import blp as corpora
-from apis.users import blp as users
 from apis.roles import blp as roles
+from apis.users import blp as users
+from core.cli import setup_cli
+from core.security import jwt
+from flask import Flask
+from settings import MONGO_CONFIG, configure_app
 from tasks import make_celery
 
 app = Flask('NERd')
-app.config['PREFERRED_URL_SCHEME'] = os.environ.get('NERD_URL_SCHEME', 'http')
-# app.config['SERVER_NAME'] = os.environ.get('NERD_SERVER_NAME', '127.0.0.1:5000')
+configure_app(app)
 
-app.config['broker_url'] = os.environ.get('BROKER_URL', 'redis://redis:6379/0')
-
-mongoengine.connect(
-    db=os.environ.get('NERD_MONGO_DB_NAME', 'nerd'),
-    host=os.environ.get('NERD_MONGO_DB_HOST', None),
-    port=os.environ.get('NERD_MONGO_DB_PORT', None),
-    username=os.environ.get('NERD_MONGO_DB_USER', None),
-    password=os.environ.get('NERD_MONGO_DB_PASSWORD', None)
-)
-
-app.config['JWT_TOKEN_LOCATION'] = ('headers', 'json')
-app.config['JWT_ERROR_MESSAGE_KEY'] = "message"
-app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'zekrit dont tell plz')  # TODO: Change secret key
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 15 * 60
-app.config['JWT_IDENTITY_CLAIM'] = 'sub'
+mongoengine.connect(**MONGO_CONFIG)
 jwt.init_app(app)
 
-app.config['OPENAPI_URL_PREFIX'] = 'api'
-app.config['OPENAPI_VERSION'] = '3.0.2'
-app.config['OPENAPI_REDOC_PATH'] = '/redoc'
-app.config['OPENAPI_REDOC_VERSION'] = 'v2.0.0-rc.4'
-app.config['OPENAPI_SWAGGER_UI_PATH'] = '/doc'
-app.config['OPENAPI_SWAGGER_UI_VERSION'] = '3.22.0'
-app.config['API_VERSION'] = '1.0.0'
-app.config['API_SPEC_OPTIONS'] = {
-    'servers': [
-        {
-            'url': '{}://{}'.format(
-                app.config['PREFERRED_URL_SCHEME'],
-                'localhost:3000'
-            ),
-            'description': 'UI endpoint'
-        },
-        {
-            'url': '{}://{}'.format(
-                app.config['PREFERRED_URL_SCHEME'],
-                os.environ.get('NERD_SERVER_NAME', '0.0.0.0:80')
-            ),
-            'description': 'Docker endpoint'
-        },
-{
-            'url': '{}://{}'.format(
-                app.config['PREFERRED_URL_SCHEME'],
-                os.environ.get('NERD_SERVER_NAME', '127.0.0.1:5000')
-            ),
-            'description': 'Local endpoint'
-        }
-    ],
-    'components': {
-        'securitySchemes': {
-            'oAuth2Password': {
-                'type': 'oauth2',
-                'description': 'Some documentation',  # TODO: Complete this
-                'flows': {
-                    'password': {
-                        'tokenUrl': '/api/auth/token',
-                        'refreshUrl': '/api/auth/refresh',
-                        'scopes': {
-                            Role.USER.value: 'User can perform queries and training',
-                            Role.ADMIN.value: 'Admin user'
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 api.init_app(app)
 api.register_blueprint(auth, url_prefix='/api/auth')
 api.register_blueprint(corpora, url_prefix='/api/corpora')
@@ -92,8 +26,7 @@ api.register_blueprint(roles, url_prefix='/api/roles')
 
 setup_cli(app)
 
-app.config['CELERY_RESULT_BACKEND'] = os.environ.get('CELERY_RESULT_BACKEND', 'redis')
-app.config['CELERY_BROKER_URL'] = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379')
+broker_url = os.environ.get('BROKER_URL', 'redis://redis:6379/0')
 
 celery = make_celery(app)
 
---
+++ app/settings.py
@@ -0,0 +1,120 @@
+
+import os
+
+from core.document.user import Role
+
+SERVERS = {
+    3000: 'UI endpoint',
+    80: 'Production endpoint',
+    5000: 'Development endpoint'
+}
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    PREFERRED_URL_SCHEME = os.environ.get('NERD_URL_SCHEME', 'http')
+    SERVER_NAME = os.environ.get('NERD_SERVER_NAME', 'localhost')
+    # SERVER_NAME = os.environ.get('NERD_SERVER_NAME', '127.0.0.1:5000')
+
+    broker_url = os.environ.get('BROKER_URL', 'redis://redis:6379/0')
+
+    CELERY_RESULT_BACKEND = os.environ.get(
+        'CELERY_RESULT_BACKEND', 'redis')
+    CELERY_BROKER_URL = os.environ.get(
+        'CELERY_BROKER_URL', 'redis://redis:6379')
+    JWT_TOKEN_LOCATION = ('headers', 'json')
+    JWT_ERROR_MESSAGE_KEY = "message"
+    JWT_SECRET_KEY = os.environ.get(
+        'JWT_SECRET_KEY', 'zekrit dont tell plz')  # TODO: Change secret key
+    JWT_ACCESS_TOKEN_EXPIRES = 15 * 60
+    JWT_IDENTITY_CLAIM = 'sub'
+
+    OPENAPI_URL_PREFIX = 'api'
+    OPENAPI_VERSION = '3.0.2'
+    OPENAPI_REDOC_PATH = '/redoc'
+    OPENAPI_REDOC_VERSION = 'v2.0.0-rc.4'
+    OPENAPI_SWAGGER_UI_PATH = '/doc'
+    OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
+    API_VERSION = '1.0.0'
+    API_SPEC_OPTIONS = {
+        'servers': [
+            {
+                'url': '{}://{}'.format(
+                    PREFERRED_URL_SCHEME,
+                    'localhost:3000'
+                ),
+                'description': 'UI endpoint'
+            },
+            {
+                'url': '{}://{}'.format(
+                    PREFERRED_URL_SCHEME,
+                    os.environ.get('NERD_SERVER_NAME', '0.0.0.0:80')
+                ),
+                'description': 'Docker endpoint'
+            },
+            {
+                'url': '{}://{}'.format(
+                    PREFERRED_URL_SCHEME,
+                    os.environ.get('NERD_SERVER_NAME', '127.0.0.1:5000')
+                ),
+                'description': 'Local endpoint'
+            }
+        ],
+        'components': {
+            'securitySchemes': {
+                'oAuth2Password': {
+                    'type': 'oauth2',
+                    'description': 'Some documentation',  # TODO: Complete this
+                    'flows': {
+                        'password': {
+                            'tokenUrl': '/api/auth/token',
+                            'refreshUrl': '/api/auth/refresh',
+                            'scopes': {
+                                Role.USER.value: 'User can perform queries and training',
+                                Role.ADMIN.value: 'Admin user'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    PREFERRED_URL_SCHEME = os.environ.get('NERD_URL_SCHEME', 'https')
+
+
+config = {
+    "development": "settings.DevelopmentConfig",
+    "testing": "settings.TestingConfig",
+    "production": "settings.ProductionConfig"
+}
+
+
+def configure_app(app):
+    config_name = os.getenv('FLASK_CONFIGURATION', 'development')
+    # object-based default configuration
+    app.config.from_object(config[config_name])
+    # instance-folders configuration
+    app.config.from_pyfile('config.cfg', silent=True)
+
+
+MONGO_CONFIG = {
+    'db': os.environ.get('NERD_MONGO_DB_NAME', 'nerd'),
+    'host': os.environ.get('NERD_MONGO_DB_HOST', None),
+    'port': os.environ.get('NERD_MONGO_DB_PORT', None),
+    'username': os.environ.get('NERD_MONGO_DB_USER', None),
+    'password': os.environ.get('NERD_MONGO_DB_PASSWORD', None)
+}
