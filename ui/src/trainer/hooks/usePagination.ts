b35c8bc886d1f1287a75d163a0a2c23eb75a6295
Juan Pablo Orsay <jporsay@gmail.com>
2019-05-11 19:06:35 -0300

    Revert use pagination change

--- ui/src/trainer/hooks/usePagination.ts
+++ ui/src/trainer/hooks/usePagination.ts
@@ -1,21 +1,24 @@
-import { useState, useEffect } from 'react';
-import useReactRouter from 'use-react-router';
-import { parse, stringify } from 'query-string';
-import { Pagination } from '../types/Pagination';
+import { useState, useEffect } from "react";
+import useReactRouter from "use-react-router";
+import { parse, stringify, parseUrl } from "query-string";
+import { Pagination } from "../types/Pagination";
 
 function buildHash(page: number, pageSize: number) {
   return {
     page,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    page_size: pageSize,
+    page_size: pageSize
   };
 }
 
 export default function usePagination() {
   const { location, history } = useReactRouter();
   const queryValues = parse(location.hash);
-  const [page, setPage] = useState<number>(queryValues.page != undefined ? +queryValues.page : 1);
-  const [pageSize, setPageSize] = useState<number>(queryValues.page_size != undefined ? +queryValues.page_size : 20);
+  const [page, setPage] = useState<number>(
+    queryValues.page != undefined ? +queryValues.page : 1
+  );
+  const [pageSize, setPageSize] = useState<number>(
+    queryValues.page_size != undefined ? +queryValues.page_size : 20
+  );
   const [total, setTotal] = useState<number>(0);
   const [shouldPaginate, setShouldPaginate] = useState<boolean>(false);
   const recordsPerPageOptions = [20, 50, 100];
@@ -25,31 +28,27 @@ export default function usePagination() {
       history.replace(location.pathname);
       return;
     }
-  }, [history, location.hash.length, location.pathname, shouldPaginate]);
+  }, [shouldPaginate]);
   useEffect(() => {
-    const newLocation = `${location.pathname}#${stringify(buildHash(page, pageSize))}`;
+    const newLocation = `${location.pathname}#${stringify(
+      buildHash(page, pageSize)
+    )}`;
     if (location.hash.length == 0 && total > 0) {
       history.replace(newLocation);
     } else {
-      if (shouldPaginate && (queryValues.page != `${page}` || queryValues.page_size != `${pageSize}`)) {
+      if (
+        shouldPaginate &&
+        (queryValues.page != `${page}` ||
+          queryValues.page_size != `${pageSize}`)
+      ) {
         history.push(newLocation);
       }
     }
-  }, [
-    history,
-    location.hash.length,
-    location.pathname,
-    page,
-    pageSize,
-    queryValues.page,
-    queryValues.page_size,
-    shouldPaginate,
-    total,
-  ]);
+  }, [page, pageSize]);
 
   function setFromHeaders(headers: any) {
-    if (headers['x-pagination'] != undefined) {
-      const pagination: Pagination = JSON.parse(headers['x-pagination']);
+    if (headers["x-pagination"] != undefined) {
+      const pagination: Pagination = JSON.parse(headers["x-pagination"]);
       setPage(pagination.page);
       setTotal(pagination.total);
       setShouldPaginate(pagination.total_pages > 1);
@@ -64,6 +63,7 @@ export default function usePagination() {
     setPage,
     setPageSize,
     recordsPerPageOptions,
-    setFromHeaders,
+    setFromHeaders
   };
 }
+
