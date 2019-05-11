8e048827b5d16356546f63e61cb95431c38605df
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2019-05-11 19:42:17 -0300

    snapshots

--- app/nerd/apis/snapshots.py
+++ app/nerd/apis/snapshots.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 from flask.views import MethodView
 from flask_rest_api import Blueprint
 from flask_rest_api.pagination import PaginationParameters
 from marshmallow_mongoengine import ModelSchema
 from nerd.apis import jwt_and_role_required
-from nerd.core.document.snapshot import Snapshot
+from nerd.core.document.snapshot import CURRENT_ID, Snapshot
 from nerd.core.document.user import Role
+from nerd.tasks.corpus import train as train_task
 
 blp = Blueprint("snapshots", "snapshots",
                 description="Corpus snapshot operations")
@@ -36,52 +39,51 @@ class IndexResource(MethodView):
             pagination_parameters.page_size
         return Snapshot.objects.skip(skip_elements).limit(pagination_parameters.page_size)
 
-    # @jwt_and_role_required(Role.ADMIN)
-    # @blp.doc(operationId="listCorpusSnapshots")
-    # @blp.response(...)
-    # @blp.arguments(...)
-    # def post(self, payload):
-    #     ...
+
+@blp.route("/current")
+class SnapshotCurrentResource(MethodView):
+
+    @jwt_and_role_required(Role.ADMIN)
+    @blp.response(CorpusSnapshotSchema)
+    @blp.doc(operationId="")
+    def get(self):
+        return Snapshot.objects.get(id=CURRENT_ID)
+
+    @jwt_and_role_required(Role.ADMIN)
+    @blp.doc(operationId="createCorpusSnapshot")
+    @blp.arguments(CorpusSnapshotSchema)
+    @blp.response(CorpusSnapshotSchema)
+    def put(self, payload):
+        current_snapshot = Snapshot.current
+        new_snapshot = Snapshot(types=current_snapshot.types)
+        new_snapshot.save()
+        current_snapshot.types = payload.types
+        current_snapshot.created_at = datetime.now()
+        current_snapshot.save()
+        return current_snapshot
 
 
-# @blp.route("/current")
-# @blp.route("/<int:snapshot_id>")
-# class SnapshotResource(MethodView):
-#
-#     @jwt_and_role_required(Role.ADMIN)
-#     @blp.response(...)
-#     @blp.doc(operationId="")
-#     def get(self, snapshot_id: int = None):
-#         ...
+@blp.route("/current")
+@blp.route("/<int:snapshot_id>")
+class SnapshotResource(MethodView):
+
+    @jwt_and_role_required(Role.ADMIN)
+    @blp.response(CorpusSnapshotSchema)
+    @blp.doc(operationId="")
+    def get(self, snapshot_id: int = None):
+        try:
+            return Snapshot.objects.get(id=snapshot_id)
+        except DoesNotExist:
+            raise NotFound()
 
 
-# @blp.route("/current/entity-types")
-# @blp.route("/<int:snapshot_id>/entity-types")
-# class EntityTypesResource(MethodView):
-#
-#     @jwt_and_role_required(Role.ADMIN)
-#     @blp.response(...)
-#     @blp.doc(operationId="")
-#     def get(self, snapshot_id: int = None):
-#         ...
+@blp.route("/force-train")
+class ForceTrainingResource(MethodView):
 
-
-# @blp.route("/current/entity-types/<string:type_name>")
-# @blp.route("/<int:snapshot_id>/entity-types/<string:type_name>")
-# class EntityTypeResource(MethodView):
-#
-#     @jwt_and_role_required(Role.ADMIN)
-#     @blp.response(...)
-#     @blp.doc(operationId="")
-#     def delete(self, snapshot_id: int = None, type_name: str = None):
-#         ...
-
-
-# @blp.route("/force-train")
-# class ForceTrainingResource(MethodView):
-#
-#     @jwt_and_role_required(Role.ADMIN)
-#     @blp.response(...)
-#     @blp.doc(operationId="")
-#     def post(self, payload):
-#         ...
+    @jwt_and_role_required(Role.ADMIN)
+    @blp.response(None)
+    @blp.doc(operationId="forceTrain")
+    def post(self, payload):
+        snapshot = Snapshot.current
+        train_task.apply_async([snapshot], queue='vCURRENT')
+        return "", 204
