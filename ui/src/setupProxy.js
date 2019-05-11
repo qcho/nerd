36e9e24727d9d5f9620a11a3759e8fec26ade7fb
Juan Pablo Orsay <jporsay@gmail.com>
2019-05-11 18:51:29 -0300

    Fix eslint

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,27 +1,27 @@
-import proxy from 'http-proxy-middleware';
-
-import { join } from 'path';
-import { urlencoded } from 'body-parser';
-import { existsSync, readFileSync, writeFileSync } from 'fs';
+/* eslint-disable */
+const proxy = require('http-proxy-middleware');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 async function addKeys(language, ns, keys) {
   if (keys['_t'] !== undefined) {
     delete keys['_t'];
   }
-  var languagePath = join(process.env.LOCALES_LOCATION, `${language}/${ns}.json`);
+  var languagePath = path.join(process.env.LOCALES_LOCATION, `${language}/${ns}.json`);
   var fileKeys = keys;
-  if (existsSync(languagePath)) {
-    var readJson = JSON.parse(readFileSync(languagePath, 'utf8'));
+  if (fs.existsSync(languagePath)) {
+    var readJson = JSON.parse(fs.readFileSync(languagePath, 'utf8'));
     fileKeys = { ...fileKeys, ...readJson };
   }
-  writeFileSync(languagePath, JSON.stringify(fileKeys, null, 2));
+  fs.writeFileSync(languagePath, JSON.stringify(fileKeys, null, 2));
 }
 
-export default function(app) {
+module.exports = function(app) {
   app.use(proxy('/api', { target: process.env.API_URL, changeOrigin: true }));
-  app.use('/locales/add/:language/:ns', urlencoded({ extended: true }), function(req, res, next) {
+  app.use('/locales/add/:language/:ns', bodyParser.urlencoded({ extended: true }), function(req, res, next) {
     const { language, ns } = req.params;
     addKeys(language, ns, req.body);
     next();
   });
-}
+};
