ddb273a75ce61a70a6e15e81793e534be91482bb
Juan Pablo Orsay <jporsay@gmail.com>
2019-06-17 18:54:52 -0300

    Created SnapshotInfo schema
    SnapshotInfo schema bundles corpus and snapshot information

--- app/nerd/apis/snapshots.py
+++ app/nerd/apis/snapshots.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from flask.views import MethodView
 from flask_rest_api import Blueprint
 from flask_rest_api.pagination import PaginationParameters
-from marshmallow_mongoengine import ModelSchema
+from marshmallow_mongoengine import ModelSchema, fields
 from mongoengine import DoesNotExist
 from werkzeug.exceptions import NotFound
 
-from nerd.apis import jwt_and_role_required, response_error
-from nerd.core.document.snapshot import CURRENT_ID, Snapshot
+from nerd.apis import jwt_and_role_required, response_error, BaseSchema
+from nerd.core.document.corpus import Text, TrainedText
+from nerd.core.document.snapshot import CURRENT_ID, Snapshot, SnapshotSchema
 from nerd.core.document.user import Role
 from nerd.tasks.corpus import reload as reload_task
 from nerd.tasks.corpus import train as train_task
@@ -17,6 +18,11 @@ blp = Blueprint("snapshots", "snapshots",
                 description="Corpus snapshot operations")
 
 
+class SnapshotInfoSchema(BaseSchema):
+    snapshot = fields.Nested(SnapshotSchema, required=True)
+    corpus_size = fields.Integer(required=True)
+    trained_texts = fields.Integer(required=True)
+    trainings = fields.Integer(required=True)
 
 
 class CreateCorpusSnapshotSchema(ModelSchema):
@@ -42,14 +48,44 @@ class IndexResource(MethodView):
         return snapshots.skip(skip_elements).limit(pagination_parameters.page_size)
 
 
+def snapshot_info(snapshot_id):
+    snapshot: Snapshot = Snapshot.objects.get(id=snapshot_id)
+    corpus_size = Text.objects(created_at__lte=snapshot.created_at).count()
+    tt = TrainedText.objects(created_at__lte=snapshot.created_at)
+    trained = tt.count()
+    # TODO: Not sure if distinct brings all of the documents to memory.
+    #   We may need a better way of doing this if so.
+    trained_texts = len(tt.distinct(field="text_id"))
+    return dict(
+        snapshot=snapshot,
+        corpus_size=corpus_size,
+        trainings=trained,
+        trained_texts=trained_texts
+    )
+
+
+@blp.route("/<int:snapshot_id>")
+class SnapshotResource(MethodView):
+
+    @jwt_and_role_required(Role.ADMIN)
+    @blp.response(SnapshotInfoSchema, code=200)
+    @response_error(NotFound("Could not find snapshot with given id"))
+    @blp.doc(operationId="getSnapshotWithId")
+    def get(self, snapshot_id: int = None):
+        try:
+            return snapshot_info(snapshot_id)
+        except DoesNotExist:
+            raise NotFound()
+
+
 @blp.route("/current")
 class SnapshotCurrentResource(MethodView):
 
     @jwt_and_role_required(Role.ADMIN)
-    @blp.response(CorpusSnapshotSchema)
+    @blp.response(SnapshotInfoSchema)
     @blp.doc(operationId="getCurrentSnapshot")
     def get(self):
-        return Snapshot.objects.get(id=CURRENT_ID)
+        return snapshot_info(CURRENT_ID)
 
     @jwt_and_role_required(Role.ADMIN)
     @blp.doc(operationId="createCorpusSnapshot")
@@ -64,20 +100,6 @@ class SnapshotCurrentResource(MethodView):
         current_snapshot.save()
         train_task.apply_async([current_snapshot.id])
         return current_snapshot
-
-
-@blp.route("/<int:snapshot_id>")
-class SnapshotResource(MethodView):
-
-    @jwt_and_role_required(Role.ADMIN)
-    @blp.response(CorpusSnapshotSchema, code=200)
-    @response_error(NotFound("Could not find snapshot with given id"))
-    @blp.doc(operationId="getSnapshotWithId")
-    def get(self, snapshot_id: int = None):
-        try:
-            return Snapshot.objects.get(id=snapshot_id)
-        except DoesNotExist:
-            raise NotFound()
 
 
 @blp.route("/force-train")
