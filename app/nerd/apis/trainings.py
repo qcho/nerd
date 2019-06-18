ec943962e1fa155aeab332d6d9715ce509597b62
Juan Pablo Orsay <jporsay@gmail.com>
2019-06-18 00:32:37 -0300

    Fix FK deletion

--- app/nerd/apis/trainings.py
+++ app/nerd/apis/trainings.py
@@ -5,8 +5,8 @@ from werkzeug.exceptions import NotFound
 
 from nerd.apis import response_error, jwt_and_role_required
 from nerd.apis.schemas import TrainingSchema
-from nerd.core.document.corpus import Training
-from nerd.core.document.user import Role
+from nerd.core.document.corpus import Training, Text
+from nerd.core.document.user import Role, User
 
 blp = Blueprint("trainings", "trainings", description="Corpus trainings operations")
 
@@ -21,6 +21,8 @@ class TrainingView(MethodView):
     def delete(self, training_id):
         try:
             training = Training.objects.get(id=training_id)
+            Text.objects.get(id=training.text_id.id).update(pull__trainings=training_id)
+            User.objects.get(id=training.user_id.id).update(pull__trainings=training_id)
             training.delete()
             return training
         except (DoesNotExist, ValidationError):
