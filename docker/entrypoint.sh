8105ed670eddba9a0c6ad956b1b47a398e939279
Juan Pablo Orsay <jporsay@gmail.com>
2019-07-03 00:38:42 -0300

    All workers start listening to vCURRENT queue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,7 +17,7 @@ main() {
             exec $HUPPER gunicorn.app.wsgiapp -k egg:meinheld#gunicorn_worker -c "/gunicorn_conf.py" "nerd:app"
             return;;
         "worker")
-            exec $HUPPER celery worker -A "nerd:celery"
+            exec $HUPPER celery worker -A "nerd:celery" -n worker@%h --queues nerd,vCURRENT
             return;;
         "flower")
             exec $HUPPER celery flower -A "nerd:celery"
