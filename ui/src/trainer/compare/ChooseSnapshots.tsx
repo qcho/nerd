dc5a12f7e21e89d345ce953dad173a19f86094bf
Juan Pablo Orsay <jporsay@gmail.com>
2019-10-13 14:12:54 -0300

    Remove onlyDifferentValues

--- ui/src/trainer/compare/ChooseSnapshots.tsx
+++ ui/src/trainer/compare/ChooseSnapshots.tsx
@@ -20,11 +20,7 @@ const ChooseSnapshots = ({ onChange }: Props) => {
   const options = availableSnapshots.map(it => {
     return { value: it, label: it };
   });
-  return (
-    <div>
-      {options.length > 0 && <DistinctValueChooser options={options} onChoose={onChoose} onlyDifferentValues />}
-    </div>
-  );
+  return <div>{options.length > 0 && <DistinctValueChooser options={options} onChoose={onChoose} />}</div>;
 };
 
 export { ChooseSnapshots };
