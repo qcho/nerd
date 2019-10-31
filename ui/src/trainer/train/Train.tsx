caa9d6d78fe1b6f631fc5ff6303d7dba1018ed6f
Juan Pablo Orsay <jporsay@gmail.com>
2019-10-30 23:45:19 -0300

    Add help section

--- ui/src/trainer/train/Train.tsx
+++ ui/src/trainer/train/Train.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Theme, Toolbar, Typography, AppBar, Button } from '@material-ui/core';
+import { Paper, Theme, Toolbar, Typography, AppBar, Button, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { useTranslation } from 'react-i18next';
 import { CorpusApi, SpacyDocument, NerApi } from '../apigen';
@@ -10,6 +10,7 @@ import { clone } from '../helpers/utils';
 import { Scaffold } from '../scaffold/Scaffold';
 import { ErrorSnackbar } from '../widgets/Snackbars';
 import Http from '../helpers/http';
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -24,18 +25,18 @@ const useStyles = makeStyles(
       flex: 1,
       flexDirection: 'column',
       [theme.breakpoints.down('sm')]: {
-        marginTop: theme.spacing.unit * 3,
+        marginTop: theme.spacing.unit * 2,
         width: '90vw',
       },
       [theme.breakpoints.up('md')]: {
-        marginTop: theme.spacing.unit * 6,
+        marginTop: theme.spacing.unit * 4,
         width: '80vw',
       },
       [theme.breakpoints.up('lg')]: {
-        marginTop: theme.spacing.unit * 8,
+        marginTop: theme.spacing.unit * 6,
         width: '60vw',
       },
-      marginBottom: theme.spacing.unit * 8,
+      marginBottom: theme.spacing.unit * 6,
       padding: theme.spacing.unit * 2,
     },
     spacer: {
@@ -47,6 +48,7 @@ const useStyles = makeStyles(
     actions: {
       display: 'flex',
       flex: 1,
+      alignItems: 'center',
       width: '100%',
       flexDirection: 'row',
       justifyContent: 'space-between',
@@ -63,6 +65,7 @@ const Train = () => {
   const [spacyDocument, setSpacyDocument] = useState<MaybeSpacyDocument>(null);
   const [noMoreDocuments, setNoMoreDocuments] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
+  const [showHelp, setShowHelp] = useState<boolean>(false);
   const api = new CorpusApi(apiConfig());
   const nerApi = new NerApi(apiConfig());
   let unmounted = false;
@@ -149,40 +152,71 @@ const Train = () => {
         )}
         {loading && !spacyDocument && <Typography>{t('Loading')}</Typography>}
         {spacyDocument && trainText && (
-          <Paper className={classes.paper}>
-            <AppBar color="default" position="relative" className={classes.actionBar}>
-              <Toolbar>
-                <div className={classes.actions}>
-                  <Button color="secondary" variant="contained" disabled={!hasChanges} onClick={onReset}>
-                    <Typography
-                      style={{ paddingLeft: 80, paddingRight: 80 }}
-                      color="inherit"
-                      variant="h6"
-                      id="tableTitle"
-                    >
-                      {t('RESET')}
-                    </Typography>
-                  </Button>
-                  <div style={{ width: '1em' }} />
-                  <Button color="primary" variant="contained" onClick={onSave} disabled={loading}>
-                    <Typography
-                      style={{ paddingLeft: 80, paddingRight: 80 }}
-                      color="inherit"
-                      variant="h6"
-                      id="tableTitle"
-                    >
-                      {t('ACCEPT')}
-                    </Typography>
-                  </Button>
-                </div>
-              </Toolbar>
-            </AppBar>
-            <TokenizedEditor
-              spacyDocument={{ ...spacyDocument }}
-              entityTypes={trainText.snapshot.types || {}}
-              onUpdate={onDocumentUpdate}
-            />
-          </Paper>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: '2em' }}>
+              <Typography variant="h4">{t('Click or drag the words to start fixing!')}</Typography>
+            </div>
+            <Paper className={classes.paper}>
+              <AppBar color="default" position="relative" className={classes.actionBar}>
+                <Toolbar>
+                  <div className={classes.actions}>
+                    <Button color="secondary" variant="contained" disabled={!hasChanges} onClick={onReset}>
+                      <Typography
+                        style={{ paddingLeft: 80, paddingRight: 80 }}
+                        color="inherit"
+                        variant="h6"
+                        id="tableTitle"
+                      >
+                        {t('RESET')}
+                      </Typography>
+                    </Button>
+                    <IconButton onClick={() => setShowHelp(!showHelp)}>
+                      <HelpOutlineOutlinedIcon />
+                    </IconButton>
+                    <Button color="primary" variant="contained" onClick={onSave} disabled={loading}>
+                      <Typography
+                        style={{ paddingLeft: 80, paddingRight: 80 }}
+                        color="inherit"
+                        variant="h6"
+                        id="tableTitle"
+                      >
+                        {t('ACCEPT')}
+                      </Typography>
+                    </Button>
+                  </div>
+                </Toolbar>
+              </AppBar>
+              <TokenizedEditor
+                spacyDocument={{ ...spacyDocument }}
+                entityTypes={trainText.snapshot.types || {}}
+                onUpdate={onDocumentUpdate}
+              />
+            </Paper>
+            {showHelp && (
+              <div style={{ maxWidth: '60vw', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="headline">{t('How to train')}</Typography>
+                <Typography variant="subtitle1" align="justify">
+                  {t('The objective is to supervise a program that will try to extract information from a given text')}
+                </Typography>
+                <Typography variant="subtitle1" align="justify">
+                  {t(
+                    'If you spot something wrong, you can remove what the program detected by clicking it and pressing the delete button',
+                  )}
+                </Typography>
+                <Typography variant="subtitle1" align="justify">
+                  {t("If you spot something that's mis-labeled, click it and choose the label that better fits it")}
+                </Typography>
+                <Typography variant="subtitle1" align="justify">
+                  {t(
+                    'If you want to add a label, click on any word or drag through a group of words to create a new word group and assign it a type',
+                  )}
+                </Typography>
+                <Typography variant="subtitle1" align="justify">
+                  {t('Once there are no more changes to make, click the ACCEPT button to submit the text')}
+                </Typography>
+              </div>
+            )}
+          </div>
         )}
         <ErrorSnackbar message={errorMessage} onClose={() => setErrorMessage('')} />
       </div>
